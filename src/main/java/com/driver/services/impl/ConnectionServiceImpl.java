2b23889e47e4e7000bfb5e517f83862f92d39c64
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-06-25 20:23:16 +0530

    4th

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -23,61 +23,121 @@ public class ConnectionServiceImpl implements ConnectionService {
     public User connect(int userId, String countryName) throws Exception{
         //Connect the user to a vpn by considering the following priority order.
         //1. If the user is already connected to any service provider, throw "Already connected" exception.
-        //2. Else if the countryName corresponds to the original country of the user, do nothing. This means that the user wants to connect to its original country,
-        //    for which we do not require a connection. Thus, return the user as it is.
+        //2. Else if the countryName corresponds to the original country of the user, do nothing. This means that the user wants to connect to its original country, for which we do not require a connection. Thus, return the user as it is.
         //3. Else, the user should be subscribed under a serviceProvider having option to connect to the given country.
         //If the connection can not be made (As user does not have a serviceProvider or serviceProvider does not have given country, throw "Unable to connect" exception.
-        //Else, establish the connection where the maskedIp is "updatedCountryCode.serviceProviderId.userId" and return the updated user.
-        // If multiple service providers allow you to connect to the country, use the service provider having smallest id.
-
+        //Else, establish the connection where the maskedIp is "updatedCountryCode.serviceProviderId.userId" and return the updated user. If multiple service providers allow you to connect to the country, use the service provider having smallest id.
         User user = userRepository2.findById(userId).get();
 
-        if(user.getConnected()){
+        if(user.getMaskedIp()!=null)
             throw new Exception("Already connected");
-        }
-        if(user.getOriginalCountry().getCountryName().toString().equals(countryName)){
+        else if (countryName.equalsIgnoreCase(user.getOriginalCountry().getCountryName().toString())) {
             return user;
-        }
+        } else{
+            if(user.getServiceProviderList()==null){
+                throw new Exception("Unable to connect");
+            }
 
-        List<ServiceProvider> serviceProviderList = user.getServiceProviderList();
+            List<ServiceProvider> providers = user.getServiceProviderList();
+            int min = Integer.MAX_VALUE;
+            ServiceProvider serviceProvider1 = null;
+            Country country1 = null;
 
-        String code = "";
-        int smallestServiceProviderId = Integer.MAX_VALUE;
-        Country getCountry = null;
+            for(ServiceProvider serviceProvider:providers){
+                List<Country> countryList = serviceProvider.getCountryList();
 
-        for(ServiceProvider serviceProvider : serviceProviderList){
-            List<Country> countryList = serviceProvider.getCountryList();
-            for(Country country : countryList) {
-                if (country.getCountryName().toString().equals(countryName)) {
-                    getCountry = country;
-                    smallestServiceProviderId = Math.min(serviceProvider.getId(), smallestServiceProviderId);
-                    code = country.getCode();
+                for (Country country:countryList){
+
+                    if(countryName.equalsIgnoreCase(country.getCountryName().toString()) && min>serviceProvider.getId()){
+                        min=serviceProvider.getId();
+                        serviceProvider1=serviceProvider;
+                        country1=country;
+                    }
                 }
             }
+            if(serviceProvider1!=null){
+                Connection connection = new Connection();
+                connection.setUser(user);
+                connection.setServiceProvider(serviceProvider1);
+
+                String countryCode = country1.getCode();
+                int providerId = serviceProvider1.getId();
+                String masked = countryCode + "." + providerId +"."+ userId;
+
+                user.setMaskedIp(masked);
+                user.setConnected(true);
+                user.getConnectionList().add(connection);
+
+                serviceProvider1.getConnectionList().add(connection);
+
+                userRepository2.save(user);
+                serviceProviderRepository2.save(serviceProvider1);
+
+                return user;
+            }
+            else
+                throw new Exception("Unable to connect");
         }
-
-        if(smallestServiceProviderId == Integer.MAX_VALUE)throw new Exception("Unable to connect");
-
-        ServiceProvider serviceProvider = serviceProviderRepository2.findById(smallestServiceProviderId).get();
-
-        String maskedIp = code + "." + smallestServiceProviderId + "." + userId;
-
-        user.setMaskedIp(maskedIp);
-        user.setConnected(true);
-        Connection connection = new Connection();
-        connection.setServiceProvider(serviceProvider);
-        connection.setUser(user);
-
-        connection = connectionRepository2.save(connection);
-        serviceProvider.getConnectionList().add(connection);
-        user.getConnectionList().add(connection);
-
-        serviceProviderRepository2.save(serviceProvider);
-
-        return user;
-
-
     }
+//    @Override
+//    public User connect(int userId, String countryName) throws Exception{
+//        //Connect the user to a vpn by considering the following priority order.
+//        //1. If the user is already connected to any service provider, throw "Already connected" exception.
+//        //2. Else if the countryName corresponds to the original country of the user, do nothing. This means that the user wants to connect to its original country,
+//        //    for which we do not require a connection. Thus, return the user as it is.
+//        //3. Else, the user should be subscribed under a serviceProvider having option to connect to the given country.
+//        //If the connection can not be made (As user does not have a serviceProvider or serviceProvider does not have given country, throw "Unable to connect" exception.
+//        //Else, establish the connection where the maskedIp is "updatedCountryCode.serviceProviderId.userId" and return the updated user.
+//        // If multiple service providers allow you to connect to the country, use the service provider having smallest id.
+//
+//        User user = userRepository2.findById(userId).get();
+//
+//        if(user.getConnected()){
+//            throw new Exception("Already connected");
+//        }
+//        if(user.getOriginalCountry().getCountryName().toString().equals(countryName)){
+//            return user;
+//        }
+//
+//        List<ServiceProvider> serviceProviderList = user.getServiceProviderList();
+//
+//        String code = "";
+//        int smallestServiceProviderId = Integer.MAX_VALUE;
+//        Country getCountry = null;
+//
+//        for(ServiceProvider serviceProvider : serviceProviderList){
+//            List<Country> countryList = serviceProvider.getCountryList();
+//            for(Country country : countryList) {
+//                if (country.getCountryName().toString().equals(countryName)) {
+//                    getCountry = country;
+//                    smallestServiceProviderId = Math.min(serviceProvider.getId(), smallestServiceProviderId);
+//                    code = country.getCode();
+//                }
+//            }
+//        }
+//
+//        if(smallestServiceProviderId == Integer.MAX_VALUE)throw new Exception("Unable to connect");
+//
+//        ServiceProvider serviceProvider = serviceProviderRepository2.findById(smallestServiceProviderId).get();
+//
+//        String maskedIp = code + "." + smallestServiceProviderId + "." + userId;
+//
+//        user.setMaskedIp(maskedIp);
+//        user.setConnected(true);
+//        Connection connection = new Connection();
+//        connection.setServiceProvider(serviceProvider);
+//        connection.setUser(user);
+//
+//        connection = connectionRepository2.save(connection);
+//        serviceProvider.getConnectionList().add(connection);
+//        user.getConnectionList().add(connection);
+//
+//        serviceProviderRepository2.save(serviceProvider);
+//
+//        return user;
+//
+//
+//    }
     @Override
     public User disconnect(int userId) throws Exception {
         //If the given user was not connected to a vpn, throw "Already disconnected" exception.
